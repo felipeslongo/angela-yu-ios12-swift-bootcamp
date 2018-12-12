99fcf44982c4df137f2a19ffa4d9591436f2dc52
Felipe de Souza Longo <felipeslongo@gmail.com>
2018-12-12 19:18:45 -0200

    Creating I Am Rich App

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Felipe de Souza Longo on 12/12/18.
+//  Copyright © 2018 Felipe de Souza Longo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
