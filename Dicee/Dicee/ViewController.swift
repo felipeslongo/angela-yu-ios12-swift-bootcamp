db4236f48f91ee07f11d1e60d67c9124dfcff830
Felipe de Souza Longo <felipeslongo@gmail.com>
2018-12-18 00:48:16 -0200

    Dicee Created

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Felipe de Souza Longo on 18/12/18.
+//  Copyright © 2018 Felipe de Souza Longo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
