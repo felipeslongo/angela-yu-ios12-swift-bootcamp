2c4f4611b80b27dbc37a1e0fd14abf6ceecbdd67
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-03-11 19:38:22 -0300

    Implemented all notes sounds

--- Xylophone-iOS12-master/Xylophone/ VIewController.swift
+++ Xylophone-iOS12-master/Xylophone/ VIewController.swift
@@ -11,18 +11,20 @@ import AVFoundation
 
 class ViewController: UIViewController, AVAudioPlayerDelegate{
     
-    
     var player: AVAudioPlayer!
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
-
     @IBAction func notePressed(_ sender: UIButton) {
+        playSound(soundFileName: soundArray[sender.tag - 1])
+    }
+    
+    func playSound(soundFileName : String){
         
-        let url = Bundle.main.url(forResource: "note1", withExtension: "wav")!
+        let url = Bundle.main.url(forResource: soundFileName, withExtension: "wav")!
         
         do {
             player = try AVAudioPlayer(contentsOf: url)
@@ -33,10 +35,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate{
         } catch let error as Error {
             print(error.localizedDescription)
         }
-        
     }
-    
-  
 
 }
 
