0ebac25ff9a5179f366c97b148258acd2140bad7
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-03-11 22:59:26 -0300

    Restart implemented

--- Quizzler-iOS12-master/Quizzler/ViewController.swift
+++ Quizzler-iOS12-master/Quizzler/ViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class ViewController: UIViewController {
     
     //Place your instance variables here
-    
+    let allQuestions = QuestionBank()
+    var pickedAnswer : Bool = false
+    var questionNumber : Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -21,11 +23,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text = firstQuestion.questionText
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if sender.tag == 1 {
+            pickedAnswer = true
+        }
+        else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
+        questionNumber += 1
+        nextQuestion()
     }
     
     
@@ -35,19 +48,36 @@ class ViewController: UIViewController {
     
 
     func nextQuestion() {
+        if(questionNumber >= allQuestions.list.count) {
+            let alert = UIAlertController(title: "Awesome", message: "You finished the quiz! Do you want to start over ?", preferredStyle: .alert)
+            
+            let restartAction = UIAlertAction(title: "Restart", style: .default, handler: {
+                (UIAlertAction) in
+                self.startOver()
+            })
+            
+            alert.addAction(restartAction)
+            present(alert, animated: true, completion: nil)
+            return
+        }
         
+        questionLabel.text = allQuestions.list[questionNumber].questionText
     }
     
     
     func checkAnswer() {
-        
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        if correctAnswer == pickedAnswer {
+            print("You got it")
+        }
+        else {
+            print("Wrong")
+        }
     }
     
     
     func startOver() {
-       
+        questionNumber = 0
+        nextQuestion()
     }
-    
-
-    
 }
